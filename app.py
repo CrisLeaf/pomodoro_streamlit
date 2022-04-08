710c273a204a40320070ff744b1cbe47f58a5fa6
Cris Torres <cristobal_javier@hotmail.com>
2022-04-08 10:59:29 -0400

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st, time
+import streamlit as st, time, beepy
 
 st.header("Pomodoro APP de Entrenamiento\n(Para participar en el sorteo)")
 series, (col1, col2) = int(st.number_input("Número de Series:", 0, 10)), st.columns(2)
@@ -8,7 +8,7 @@ descansos = [int(col2.number_input(f"Duración descanso {i+1}", min_value=0, ste
 if st.button("Empezar"):
 	for index, (ejercicio, descanso) in enumerate(zip(ejercicios, descansos)):
 		with st.empty():
-			print("\007")
+			beep(sound="coin")
 			for i in range(ejercicio, 0, -1):
 				st.markdown(f"""<p style="color:green;font-size:50px">{i}s</p>""", unsafe_allow_html=True)
 				time.sleep(1)
